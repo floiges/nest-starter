ab839d22a78b660197c5a0500b471e499d126548
yadong <floiges@163.com>
2019-02-21 14:49:00 +0800

    custom providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,39 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { logger } from './logger.middleware';
 import { APP_FILTER } from '@nestjs/core';
+import { CatsService } from './cats/cats.service';
 // import { HttpExceptionFilter } from './exceptions/http-filter.exception';
 
 //  The @Module() decorator provides metadata that Nest makes use of to organize the application structure
 // modules are singletons by default
+
+// Custom Provider
+// useValue
+// In below example, the CatsService will be overridden by a passed mockCatsService mock object
+//  It means, that Nest instead of creating CatsService instance manually, 
+// will treat this provider as resolved already, and use mockCatsService as its representative value.
+const mockCatsService = {};
+const catsServiceProvider = {
+  provide: CatsService,
+  useValue: mockCatsService
+};
+
+// useClass
+// const configServiceProvider = {
+//   provide: configService,
+//   useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService
+// };
+
+// The useFactory is a way of creating providers dynamically.
+// const connectionFactory = {
+//   provide: "Connection",
+//   useFactory: (optionsProvider: OptionsProvider) => {
+//     const options = optionsProvider.get();
+//     return new DatabaseConnection(options);
+//   },
+//   inject: [OptionsProvider]
+// };
+
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
@@ -16,6 +45,7 @@ import { APP_FILTER } from '@nestjs/core';
     //   provide: APP_FILTER,
     //   useClass: HttpExceptionFilter //  set up a filter directly from any module
     // }, 
+    catsServiceProvider,
     AppService],
 })
 //  We have to set middleware up using the configure() method of the module class.
